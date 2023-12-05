8af64ac3877d30749a05b0cbd7d1a10756864679
MatteoSanson <matteo.sanson@gmail.com>
2023-12-05 17:15:08 +0100

    fet: prova axios email

--- main.js
+++ main.js
@@ -5,7 +5,22 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
-    }
-}
-}).mount('#app')
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            email: '',
+        };
+    },
+    methods: {
+        
+    },
+    mounted() {
+        axios.get(this.apiUrl).then((response) => {
+            this.email = response.data.response;
+    });
+},
+}).mount('#app');
+
+
+// const emails = axios.get(
+//     'https://flynn.boolean.careers/exercises/api/random/mail'
+//     ).then ((response) => 
+//     console.log(response.data.response));
